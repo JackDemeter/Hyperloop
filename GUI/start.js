bd9e0c27a40dea4813f46daa53841480f97cb69e
Jack Demeter <jack.demeter@queensu.ca>
2019-03-01 21:33:14 -0500

    moved gui

--- GUI/start.js
+++ GUI/start.js
@@ -1,9 +1,10 @@
 //start.js
 var http  = require('http');
 var spawn = require('child_process').spawn;
-var fs = require('fs');
+var fs    = require('fs');
 var py    = spawn('python', ['./hyperloop_collection.py']);
 var dataString = ""
+var url   = require('url');
 
 py.stdout.on('data', function(data){
   dataString = data.toString();
@@ -18,5 +19,12 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data); //write a response to the client
     res.end(); //end the response
+    var url_parts = url.parse(req.url, false);
+    if (url_parts.query == "EmergencyStop=True")
+    {
+      console.log("EMERGENCY STOP ENGAGED");
+      
+    }
+
   });
 }).listen(8080); //the server object listens on port 8080
