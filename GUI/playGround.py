0029bc24eaae9fff6938b3608f2d319f741edd52
Jack Demeter <jack.demeter@queensu.ca>
2019-03-16 02:34:58 -0400

    got some stuff working

--- GUI/playGround.py
+++ GUI/playGround.py
@@ -5,61 +5,11 @@ playground for messing around and testing the PyQT5 library
 
 """
 
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QMessageBox, QAction, QTableWidget, QTableWidgetItem, QVBoxLayout, QSizePolicy, QProgressBar, QLayout, QGridLayout, QDockWidget
-from PyQt5.QtGui import QIcon, QPixmap
+import sys, os
+from PyQt5.QtWidgets import QApplication, QWidget, QTableWidget, QMainWindow, QPushButton, QMessageBox, QAction, QTableView, QTableWidgetItem, QVBoxLayout, QSizePolicy, QProgressBar, QLabel
+from PyQt5.QtGui import QIcon, QPixmap, QStatusTipEvent
 from PyQt5.QtCore import pyqtSlot, QSize
-
-# QMain window used for status bar
-# QMessageBox to bring in pop ups
-
-class WidgetTable(QDockWidget):
-    def __init__(self, left = 0, top = 0, width = 100, height = 100):
-        super().__init__()
-        self.title = 'PyQt5 table - pythonspot.com'
-        self.left = left
-        self.top = top
-        self.width = width
-        self.height = height
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-
-        self.createTable()
-
-        # Add box layout, add table to box layout and add box layout to widget
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.tableWidget)
-        self.setLayout(self.layout)
-
-        # Show widget
-        self.show()
-
-    def createTable(self):
-        # Create table
-        self.tableWidget = QTableWidget()
-        self.tableWidget.setRowCount(4)
-        self.tableWidget.setColumnCount(2)
-        self.tableWidget.setItem(0, 0, QTableWidgetItem("Cell (1,1)"))
-        self.tableWidget.setItem(0, 1, QTableWidgetItem("Cell (1,2)"))
-        self.tableWidget.setItem(1, 0, QTableWidgetItem("Cell (2,1)"))
-        self.tableWidget.setItem(1, 1, QTableWidgetItem("Cell (2,2)"))
-        self.tableWidget.setItem(2, 0, QTableWidgetItem("Cell (3,1)"))
-        self.tableWidget.setItem(2, 1, QTableWidgetItem("Cell (3,2)"))
-        self.tableWidget.setItem(3, 0, QTableWidgetItem("Cell (4,1)"))
-        self.tableWidget.setItem(3, 1, QTableWidgetItem("Cell (4,2)"))
-        self.tableWidget.move(0, 0)
-
-        # table selection change
-        self.tableWidget.doubleClicked.connect(self.on_click)
-
-    @pyqtSlot()
-    def on_click(self):
-        print("\n")
-        for currentQTableWidgetItem in self.tableWidget.selectedItems():
-            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
+import widget
 
 
 class App(QMainWindow):
@@ -73,65 +23,69 @@ class App(QMainWindow):
         self.windowIcon()
         self.ping = 5
         self.connectionPort = "No connection"
+        self.status = "please"
+
+        # Create a menu
+        self.mainMenu = self.menuBar()
+        self.commandMenu = self.mainMenu.addMenu('Commands')
+        self.connectMenu = self.mainMenu.addMenu('Connection')
+        self.viewMenu = self.mainMenu.addMenu('View')
+
+        # Create a menu action
+        self.exitButton = QAction('Exit', self)
+        self.specsButton = QAction('Specs', self)
+        self.emergencyButton = QAction('Emergency Stop', self)
+
+        # calls GUI
         self.initUI()
 
     def initUI(self):
-
         self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        self.setWindowIcon(QIcon(r'images\QH_icon.png'))
+        self.setFixedSize(self.width, self.height)
+        self.setWindowIcon(QIcon(os.getcwd() + r'\images\QH_icon.png'))
 
-        # self.addDockWidget()
-        # add widget table
-        table = WidgetTable(100, 100,200,200)
-        self.setCentralWidget(table)
-
-        # create a button
-        button = QPushButton('Emergency\nStop', self)
-        button.setStyleSheet("color:black")
-        button.setToolTip('This button engages the pods fault state')
-        button.move(1080, 30)
-        # colour/style button
-        button.setStyleSheet("background-color:rgb(230,80,80); color:black; font-size:25px")
-        button.resize(200,70)
-        # link button to pyqtslot
-        button.clicked.connect(self.on_click)
-
-        # Create a menu
-        mainMenu = self.menuBar()
-        fileMenu = mainMenu.addMenu('File')
-        editMenu = mainMenu.addMenu('Edit')
-        viewMenu = mainMenu.addMenu('View')
-        # Extra menu settings
-        exitButton = QAction(QIcon(r'images\QH_icon.png'), 'Exit', self)
-        exitButton.setShortcut('Ctrl+Q')
-        exitButton.setStatusTip('Exit application')
-        exitButton.triggered.connect(self.close)
-        fileMenu.addAction(exitButton)
         # Create a status bar at the bottom of the page
-        self.statusBar().showMessage('Ping: %dms              Port: %s              Other: %s' %(self.ping, self.connectionPort,"None"))
+        self.statusBar().showMessage('Ping: %dms              Port: %s              Other: %s' % (self.ping, self.connectionPort, "None"))
+        # TODO create a slot that constantly refreshes this
 
-        # create a progress bar
-        progress = QProgressBar(self)
-        progress.setGeometry(450, 150, 800, 15)
-        progress.setValue(50)
+        # Extra menu settings
+        self.exitButton.setShortcut('Ctrl+Q')
+        self.exitButton.setStatusTip('Exit application')
+        self.commandMenu.addAction(self.exitButton)
+        self.exitButton.triggered.connect(self.close)
+
+        self.specsButton.setShortcut('Ctrl+V')
+        self.specsButton.setStatusTip('Look at the harder specs')
+        self.viewMenu.addAction(self.specsButton)
+        self.specsButton.triggered.connect(self.specs)
+
+        self.emergencyButton.setShortcut('Ctrl+E')
+        self.emergencyButton.setStatusTip('Go into an emergency stop')
+        self.commandMenu.addAction(self.emergencyButton)
+        self.emergencyButton.triggered.connect(self.emergency)
 
 
-
-
-
+        #set our widget as the main box
+        self.setCentralWidget(widget.App(1280,720, self))
         self.show()
 
+        """
         # create pop up button button
         buttonReply = QMessageBox.question(self, 'Connection', "Connection established with the pod", QMessageBox.Ok, QMessageBox.Cancel)
         if buttonReply == QMessageBox.Ok:
             print('Accepted.')
+        """
+
+    def specs(self):
+        print("kk")
+        # TODO show any hidden data ? new window?
 
 
     # creates a slot for the button
     @pyqtSlot()
-    def on_click(self):
+    def emergency(self):
         print('Fault State Engaged')
+        self.status = "FAULT"
         # TODO engage the fault state on pod
 
 
--- GUI/widget.py
+++ GUI/widget.py
@@ -1,29 +1,88 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QMenuBar, QStatusBar, QPushButton, QMessageBox, QAction, QTableView, QTableWidget, QTableWidgetItem, QVBoxLayout, QSizePolicy, QProgressBar, QLabel, QVBoxLayout, QHBoxLayout, QLayout, QGridLayout, QComm
 from PyQt5.QtGui import QIcon, QPixmap
-
+from PyQt5.QtCore import pyqtSlot, QSize
 
 class App(QWidget):
-    def __init__(self):
+    def __init__(self, width, height, mainWindow):
         super().__init__()
-        self.title = 'PyQt5 image - pythonspot.com'
-        self.left = 10
-        self.top = 10
-        self.width = 3000
-        self.height = 480
+        self.title = "Queen's Hyperloop Design Team"
+        self.left = 600
+        self.top = 200
+        self.width = width
+        self.height = height
+        self.ping = 5
+        self.connectionPort = "No connection"
+        self.status = "RTL"
+        self.mainWindow = mainWindow
+
+        self.tableWidget = QTableWidget(4, 2)
+        # self.layout = QVBoxLayout(self)
+        self.layout = QGridLayout(self)
+        # calls GUI
         self.initUI()
+
     def initUI(self):
         self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setFixedSize(1280, 680)
 
-        # Create widget
-        label = QLabel(self)
+        # add images
+        self.titleLogo = QLabel()
         pixmap = QPixmap(r'images\QH_logo.png')
-        label.setPixmap(pixmap)
+        pixmap = pixmap.scaled(400, 100)
+        self.titleLogo.setPixmap(pixmap)
+        self.titleLogo.resize(300,200)
+        self.layout.addWidget(self.titleLogo,0,0)
 
-        self.show()
+        self.sample = QLabel()
+        pixmap = QPixmap(r'images\example.png')
+        pixmap = pixmap.scaled(500, 600)
+        self.sample.setPixmap(pixmap)
+        self.sample.resize(500, 400)
+        self.layout.addWidget(self.sample,1,1)
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-ex = App()
-sys.exit(app.exec_())
+
+        # adds table widget
+        self.tableWidget.setItem(0, 0, QTableWidgetItem("Cell (1,1)"))
+        self.tableWidget.setItem(0, 1, QTableWidgetItem("Cell (1,2)"))
+        self.tableWidget.setItem(1, 0, QTableWidgetItem("Cell (2,1)"))
+        self.tableWidget.setItem(1, 1, QTableWidgetItem("Cell (2,2)"))
+        self.tableWidget.setItem(2, 0, QTableWidgetItem("Cell (3,1)"))
+        self.tableWidget.setItem(2, 1, QTableWidgetItem("Cell (3,2)"))
+        self.tableWidget.setItem(3, 0, QTableWidgetItem("Cell (4,1)"))
+        self.tableWidget.setItem(3, 1, QTableWidgetItem("Cell (4,2)"))
+        self.tableWidget.setFixedSize(217,400)
+        self.layout.addWidget(self.tableWidget,1,0)
+
+
+
+
+        # add label for state
+        self.Statelabel = QLabel("State: {}".format(self.status), self)
+        self.Statelabel.move(800, 00)
+        self.Statelabel.setStyleSheet("color:black; font-size:40px")
+        self.Statelabel.resize(250, 70)
+        # create a button
+        button = QPushButton('Emergency\nStop', self)
+        button.setStyleSheet("color:black")
+        button.setToolTip('This button engages the pods fault state')
+        button.move(1080, 0)
+        # colour/style button
+        button.setStyleSheet("background-color:rgb(230,80,80); color:black; font-size:25px")
+        button.resize(200,70)
+        # link button to pyqtslot
+        button.clicked.connect(self.mainWindow.emergency)
+
+        # create a progress bar
+        progress = QProgressBar(self)
+        progress.setGeometry(450, 100, 800, 10)
+        progress.setValue(50)
+
+
+
+
+    # creates a slot for the button
+    @pyqtSlot()
+    def on_click(self):
+        print('Fault State Engaged')
+        # TODO engage the fault state on pod
