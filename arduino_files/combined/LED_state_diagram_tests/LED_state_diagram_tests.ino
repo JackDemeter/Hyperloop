8a2cb2643bf801641fa0efc2412ecedb59acc155
JackDemeter <jack.demeter@hotmail.com>
2019-11-20 15:29:12 -0500

    Added additional pin support, implemented time keeping in check function

--- arduino_files/combined/LED_state_diagram_tests/LED_state_diagram_tests.ino
+++ arduino_files/combined/LED_state_diagram_tests/LED_state_diagram_tests.ino
@@ -45,29 +45,38 @@ struct StateDetails
 //char telemetry[PACKET_SIZE];
 
 /****     PIN LAYOUT      ****/
-int led_0 = 2;
-int led_1 = 3;
-int led_2 = 4;
+int led_0 = 50;
+int led_1 = 51;
+int led_2 = 52;
 
-int RTL_switch = 7;
+int motor_pin = 3;
+int brake_pin = 4;
+
+int RTL_switch = 10;
+int power_led = 11;
+int network_led = 12;
+int safety_led = 13;
 
 
 
 float pressure;
 unsigned long pulseWidth;
 
-int receivedState;
 bool initialBrake;
 
 // Create current/previous states allows for one shot programming later in the code
 state currentState, prevState;
 // Time State initiated, used for time based thresholding
-unsigned long TSI;
+unsigned long long *TSI;
 // the time that the loop initiated
 unsigned long startTime;
 
 void setup(void)
 {
+  // initialize power first
+  pinMode(power_led, OUTPUT);
+  digitalWrite(power_led, HIGH);
+  
   pinMode(RTL_switch, INPUT);
   //  pinMode(RTL_switch_reset, OUTPUT);
 
@@ -75,12 +84,15 @@ void setup(void)
   pinMode(led_1, OUTPUT);
   pinMode(led_2, OUTPUT);
 
+  pinMode(network_led, OUTPUT);
+
   pinMode(RTL_switch, INPUT);
 
   //init all lights to high
   digitalWrite(led_0, HIGH);
   digitalWrite(led_1, HIGH);
   digitalWrite(led_2, HIGH);
+  digitalWrite(network_led, LOW);
 
   // initiate default state on start up
   //  receivedState = state::STA;
@@ -104,17 +116,18 @@ void setup(void)
     dispState(i);
     delay(500);
   }
+  digitalWrite(network_led, HIGH);
 }
 
 
 void loop(void) {
   // Update state based on network info
   state recvState = getSerialState();
-  currentState = checkState(recvState, currentState);
+  currentState = checkState(recvState, currentState, TSI);
   dispState(currentState);
 }
 
-state getSerialState()
+state getNetworkState()
 {
   char receivedChar;
   if (Serial3.available())
@@ -140,7 +153,7 @@ state checkState(state receivedState) {
     return receivedState;
 }
 
-state checkState(state receivedState, state currentState)
+state checkState(state receivedState, state currentState, unsigned long long *TSI)
 {
   // check state (switch case)
   switch (currentState)
@@ -149,48 +162,107 @@ state checkState(state receivedState, state currentState)
       {
         if (digitalRead(RTL_switch) && receivedState == state::RTL)
         {
+          *TSI = millis();
+          Serial.print("TSI: ");
+          Serial.println(TSI); 
           return state::RTL;
         }
         break;
       }
     case (state::RTL):
       {
-        if (receivedState == state::STA)return state::STA;
-        if (receivedState == state::LAUNCH)return state::LAUNCH;
+        if (receivedState == state::STA)
+        {
+          *TSI = millis();
+          Serial.print("TSI: ");
+          Serial.println(TSI); 
+          return state::STA;
+        }
+        if (receivedState == state::LAUNCH)
+        {
+          *TSI = millis();
+          Serial.print("TSI: ");
+          Serial.println(TSI); 
+          return state::LAUNCH;
+        }
         break;
       }
     case (state::LAUNCH):
       {
-        if (receivedState == state::COAST)return state::COAST;
-        if (receivedState == state::BRAKE)return state::BRAKE;
+        if (receivedState == state::COAST)
+        {
+          *TSI = millis();
+          Serial.print("TSI: ");
+          Serial.println(TSI); 
+          return state::COAST;
+        }
+        if (receivedState == state::BRAKE)
+        {
+          *TSI = millis();
+          Serial.print("TSI: ");
+          Serial.println(TSI); 
+          return state::BRAKE;
+        }
         break;
       }
     case (state::COAST):
       {
-        if (receivedState == state::BRAKE)return state::BRAKE;
-        if (receivedState == state::CRAWL)return state::CRAWL;
+        if (receivedState == state::BRAKE)
+        {
+          *TSI = millis();
+          Serial.print("TSI: ");
+          Serial.println(TSI); 
+          return state::BRAKE;
+        }
+        if (receivedState == state::CRAWL)
+        {
+          *TSI = millis();
+          Serial.print("TSI: ");
+          Serial.println(TSI); 
+          return state::CRAWL;
+        }
         break;
       }
     case (state::BRAKE):
       {
-        if (receivedState == state::CRAWL) return state::CRAWL;
-        if (receivedState == state::STA) return state::STA;
+        if (receivedState == state::CRAWL) 
+        {
+          *TSI = millis();
+          Serial.print("TSI: ");
+          Serial.println(TSI); 
+          return state::CRAWL;
+        }
+        if (receivedState == state::STA) 
+        {
+          *TSI = millis();
+          Serial.print("TSI: ");
+          Serial.println(TSI); 
+          return state::STA;
+        }
         break;
       }
     case (state::CRAWL):
       {
-        if (receivedState == state::BRAKE) return state::BRAKE;
+        if (receivedState == state::BRAKE) 
+        {
+          *TSI = millis();
+          Serial.print("TSI: ");
+          Serial.println(TSI); 
+          return state::BRAKE;
+        }
         break;
       }
     default:
+      *TSI = millis();
+      Serial.print("TSI: ");
+      Serial.println(TSI); 
       return state::FAULT;
   }
   return currentState;
-
 }
 
 
-state getCurrentState()
+state getSerialState()
 {
   if (Serial.available()) {
     char input = Serial.read();
