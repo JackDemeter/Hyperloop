6bb58937c9d6d2a11d2ab88616ee2fd58d0b51d4
Jake <15jps1@queensu.ca>
2020-03-11 17:48:24 -0400

    Polls pod status and send via udp

--- web_based_GUI/public/javascripts/index.js
+++ web_based_GUI/public/javascripts/index.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-        //set pod-status to safe to approach.
+        // Initially set pod-status in local storage to safe to approach.
         localStorage.setItem('pod-status', 2);
 });
 
+// Set pod-status in local storage according to button press:
 $('#emerg-stop').click(function(){
         localStorage.setItem('pod-status', 0);
 });
@@ -15,6 +16,7 @@ $('#safe-approach').click(function(){
         localStorage.setItem('pod-status', 2);
 });
 
+// Set an event that polls the local storage for pod-status and calls to the back-end to transmit the appropriate status to the pod.
 setInterval(function(){
         $.post('/send-status', {status: localStorage.getItem('pod-status')});
 }, 1000);
--- web_based_GUI/routes/index.js
+++ web_based_GUI/routes/index.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
-server.connect(3000, "192.168.8.20");
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Initialize pod server
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+server.connect(3000, "192.168.8.20");
+// sends message via udp to the pod server
 function send_via_udp(message) {
-  var buf = Buffer.from(message);
-  console.log(buf);
+  var buf = Buffer.from([parseInt(message)]);
   server.send(buf);
 };
 
+// Handles posts to /send-status (executed in a loop from the front end)
 router.post('/send-status', function(req, res, next) {
   send_via_udp(req.body.status);
 });
